0a96357c70032988d44c717d95b240b1c102a274
Rohan Kommathoti <kyrohan7730@gmail.com>
2026-01-02 05:37:10 -0600

    mvp-1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,8 @@
-import React from 'react'
-import { Routes,Route } from 'react-router'
-import Home from './pages/Home'
-
+import { useState } from "react";
+import { getTokens } from "./api";
+import { AuthPage } from "./pages/AuthPage";
+import { ChatPage } from "./pages/ChatPage";
 export default function App() {
-  return (
-    <Routes>
-      <Route element={<Home/>} path="/" />
-    </Routes>
-  )
+  const [authed, setAuthed] = useState(!!getTokens());
+  return authed ? <ChatPage onLogout={() => setAuthed(false)} /> : <AuthPage onAuthed={() => setAuthed(true)} />;
 }
